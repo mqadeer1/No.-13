f9821c3ecad24cad7c4dd0098d1ede4a29361c91
mqadeer1 <m.qadeer1600@gmail.com>
2025-01-06 21:41:13 -0500

    splatter works on the dress

--- Assets/Scripts/SprayPlace.cs
+++ Assets/Scripts/SprayPlace.cs
@@ -34,31 +34,46 @@ public class SprayPlace : MonoBehaviour
     private void Update()
     {
         // Perform the raycast from the spray nozzle
-        ray = new Ray(sprayNozzle.position, -sprayNozzle.up);
-        Debug.DrawRay(ray.origin, ray.direction * sprayDistance, Color.red);
+    ray = new Ray(sprayNozzle.position, -sprayNozzle.up);
+    Debug.DrawRay(ray.origin, ray.direction * sprayDistance, Color.red);
 
-        // Check if the spray button is pressed and the raycast hits a target
-        if (sprayAction.ReadValue<float>() > 0 && Physics.Raycast(ray, out hit, sprayDistance, layerMask))
+  //  Debug.Log($"SprayPlace: Ray created from {ray.origin} in direction {ray.direction}");
+
+    // Check if the spray button is pressed and the raycast hits a target
+    if (sprayAction.ReadValue<float>() > 0)
+    {
+        Debug.Log("SprayPlace: Spray button pressed.");
+
+        if (Physics.Raycast(ray, out hit, sprayDistance, layerMask))
         {
+            Debug.Log($"SprayPlace: Raycast hit {hit.collider.name} at {hit.point}");
             PlaceDecal(hit.point, hit.normal);
         }
+        else
+        {
+            Debug.Log("SprayPlace: Raycast did not hit anything.");
+        }
+    }
     }
 
     private void PlaceDecal(Vector3 position, Vector3 normal)
     {
-        // Instantiate the decal prefab
-        GameObject decalInstance = Instantiate(decalPrefab, position, Quaternion.LookRotation(normal));
+         // Instantiate the decal prefab
+    GameObject decalInstance = Instantiate(decalPrefab, position, Quaternion.LookRotation(normal));
 
-        // Configure the Decal Projector
-        DecalProjector decalProjector = decalInstance.GetComponent<DecalProjector>();
-        if (decalProjector != null)
-        {
-            decalProjector.size = decalSize;
-        }
+    // Configure the Decal Projector
+    DecalProjector decalProjector = decalInstance.GetComponent<DecalProjector>();
+    if (decalProjector != null)
+    {
+        decalProjector.size = decalSize;
+    }
 
-        // Optional: Destroy the decal after some time to avoid clutter
-        Destroy(decalInstance, 10f);
+    // Make the decal a child of the hit object to follow its animation
+    decalInstance.transform.SetParent(hit.collider.transform, worldPositionStays: true);
 
-        Debug.Log($"Decal placed at {position}, aligned with normal {normal}");
+    // Optional: Destroy the decal after some time to avoid clutter
+    //Destroy(decalInstance, 10f);
+
+    Debug.Log($"Decal placed at {position}, aligned with normal {normal}");
     }
 }
