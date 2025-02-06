bf9cd6246efd15b69b9ef88b60bd309b5b2c3e8d
mqadeer1 <80364567+mqadeer1@users.noreply.github.com>
2025-02-06 10:08:13 -0500

    PUSG

---
+++ Assets/Scripts/CancelPlay.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CancelPlay : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("CancelPlay script is running!");
+    }
+
+    public void GoToUI()
+    {   
+         Debug.Log("Button was clicked!");
+    }
+}
