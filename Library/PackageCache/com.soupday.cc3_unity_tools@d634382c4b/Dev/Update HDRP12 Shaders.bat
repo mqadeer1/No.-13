7bdf772737cf65ded72459f770a8f09cc8e485d9
Mahum Qadeer <mahumqadeer@Mahums-MacBook-Pro.local>
2025-02-06 14:42:41 -0500

    Removed Library from tracking

--- Library/PackageCache/com.soupday.cc3_unity_tools@d634382c4b/Dev/Update HDRP12 Shaders.bat
+++
@@ -1,4 +0,0 @@
-copy /y ".\..\HDRP\Shaders\RL_HairShader_Baked_HDRP.shadergraph" ".\..\HDRP\Shaders\HDRP12\RL_HairShader_Baked_HDRP12.shadergraph"
-copy /y ".\..\HDRP\Shaders\RL_HairShader_Variants_HDRP.shadergraph" ".\..\HDRP\Shaders\HDRP12\RL_HairShader_Variants_HDRP12.shadergraph"
-
-pause 0
